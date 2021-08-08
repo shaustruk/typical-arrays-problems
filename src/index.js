e19b85d9be6acd8f9427988b6f3f9dd386422190
KATSIARYNA <statsevich22@gmail.com>
2021-08-09 01:25:30 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
+  else {
+    const sortArr = (a, b) => a - b;
+    array = array.sort(sortArr);
+    return array[0];
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
+  else {
+    const sortArr = (a, b) => b - a;
+    array = array.sort(sortArr);
+    console.log(array);
+    return array[0];
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
+  else {
+    const reducer = (sum, arr) => sum + arr;
+    let res = array.reduce(reducer);
+    return res / array.length;
+  }
+
 }
+
